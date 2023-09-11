04cf5a8cfc5ffb7a822852636413c31d4f27f28e
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-09-11 18:14:43 -0500

    Gather data from user, add buttons to remove books and toggle read

--- javascript.js
+++ javascript.js
@@ -1,34 +1,71 @@
 const myLibrary = [];
 
-function Book(title, author, num_pages, have_read) {
+function Book(title, author, numPages, haveRead) {
     this.title = title;
     this.author = author;
-    this.num_pages = num_pages;
-    this.have_read = have_read;
+    this.numPages = numPages;
+    this.haveRead = haveRead;
     this.info = function () {
-        return `${title} by ${author}, ${num_pages} pages, ${have_read ? 'read' : 'not read yet'}`;
+        return `${title} by ${author}, ${numPages} pages, ${haveRead ? 'read' : 'not read yet'}`;
     };
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(title, author, numPages, haveRead) {
+    const book = new Book(title, author, numPages, haveRead);
     myLibrary.push(book);
 }
 
-for (const book of myLibrary) {
+function displayBooks() {
+    for (const book of myLibrary) {
 
+    }
 }
 
-const book1 = new Book("Dummy Book 1", "Dum Dum1", 100, false);
-const book2 = new Book("Dummy Book 2", "Dum Dum2", 200, true);
-const book3 = new Book("Dummy Book 3", "Dum Dum3", 300, false);
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
 const addBtn = document.querySelector(".addBtn");
+const cancelBtn = document.querySelector(".cancelBtn");
+const confirmBtn = document.querySelector(".confirmBtn");
+
 const addDialog = document.querySelector(".addDialog");
+
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const yesRadio = document.querySelector("#yes");
+const noRadio = document.querySelector("#no");
+
+let cancelled = true;
 
 addBtn.addEventListener("click", () => {
     addDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => {
+    cancelled = true;
+    addDialog.close();
+});
+
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    cancelled = false;
+    addDialog.close();
+});
+
+addDialog.addEventListener("close", (value) => {
+    if (cancelled) {
+        console.log("Form was cancelled");
+    } else {
+        console.log("Form was submitted");
+        console.log(titleInput.value);
+        console.log(authorInput.value);
+        console.log(pagesInput.value);
+        console.log(yesRadio.checked);
+        console.log(noRadio.checked);
+        addBookToLibrary(titleInput.value, authorInput.value,
+            pagesInput.value, yesRadio.checked ? true : false);
+    }
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    yesRadio.checked = false;
+    noRadio.checked = false;
 });
